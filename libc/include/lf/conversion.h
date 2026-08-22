6e605084a4dd22b7655ace97f70042ffce2d0f83
yujidesombres-ctrl <yuji.des.ombres@gmail.com>
2026-08-23 01:55:19 +0200

    Remove LunaLibC file

--- libc/include/lf/conversion.h
+++
@@ -1,9 +0,0 @@
-#ifndef LF_CONVERSION_H
-#define LF_CONVERSION_H
-
-#include <stddef.h>
-
-long lf_strtol(const char *s, char **end, int base);
-unsigned long lf_strtoul(const char *s, char **end, int base);
-
-#endif
