01c46038f8361c85d125253264a91d0f22358590
yujidesombres-ctrl <yuji.des.ombres@gmail.com>
2026-08-23 01:59:06 +0200

    Remove abandoned LunaLibC files

--- libc/src/assert.c
+++
@@ -1,9 +0,0 @@
-#include <lf/assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-_Noreturn void lf_assert_fail(const char *expression, const char *file, int line)
-{
-    fprintf(stderr, "LunaLibC assertion failed: %s (%s:%d)\n", expression, file, line);
-    abort();
-}
